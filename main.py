b52da35d4eca4e24314764608f7ea9c406f71a7f
kolarjak123 <83831736+kolarjak123@users.noreply.github.com>
2021-05-08 08:41:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,13 +12,15 @@ from flask_gravatar import Gravatar
 import gunicorn
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")  # pro heroku web poziti
+# app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'   # pro PC pouziti
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", 'sqlite:///blog.db')  # pro heroku web poziti - jako prvni se pokus pouzit z enviromantal variable (heroku web) a pokud eni (lokalni pc) tak sahne alternativne po tom druhem zaznamu, tedy lokalni db
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'   # pro PC pouziti
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
